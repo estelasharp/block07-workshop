8260de3ba4d396d300f948a9ee8c50dda79661cc
Estela Sharp <estelasharp@Estelas-MacBook.local>
2023-11-06 14:31:57 -0500

    upload for completion of workshop

---
+++ scriptfile.js
@@ -0,0 +1,20 @@
+/*To begin, I will crearte a string for the user, then assign three variables, during the process I will add commentary, finally a dialog box will be added displaying vault codes*/
+const x = ('You have received this message because you have been chosen to open an important vault. Here is the secret combination:');
+console.log(x);
+/*var 10= 5 + 5;
+var 40= 4*10;
+var 39= 30+9;
+console.log(10)
+Variables didn't reflect as expected, redoing the process*/
+var a= 5 + 5;
+console.log(a);
+var b=10*4;
+console.log(b);
+var c=42-3;
+console.log(c);
+/*added a variable and a unique arithmetic operator to complete the calculation*/;
+let str0 = 'Combination Code: ';
+let str1 = '10,';
+let str2 = '40,';
+let str3 = '39';
+alert(str0  + str1 + ' ' + str2 + ' ' + str3);
